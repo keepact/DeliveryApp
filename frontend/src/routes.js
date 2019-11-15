2fda18308e08551662c2f6a965acf629d7ad9bcf
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-15 19:14:30 -0300

    push all project

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,14 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
 import List from './pages/List';
-import MapContainer from './pages/Map';
+import MapView from './pages/Map';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Main} />
       <Route path="/list" component={List} />
-      <Route path="/map" component={MapContainer} />
+      <Route path="/map" component={MapView} />
     </Switch>
   );
 }
