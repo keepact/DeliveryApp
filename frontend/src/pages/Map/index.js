a05b130dee3763b51e9e9cf1080f564c82655d7f
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-15 18:51:53 -0300

    Frotend and backend integration

--- frontend/src/pages/Map/index.js
+++ frontend/src/pages/Map/index.js
@@ -8,7 +8,7 @@ export default function MapContainer() {
   return (
     <div style={{ width: `100vw`, height: `100vh` }}>
       <WrapperMap
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCQJIaL-F_f5U8MRF4gznv1dsgmzmtHVHs"
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
