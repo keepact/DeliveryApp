ef94a98ad16d25028dfd4adb2a2e675e2a5f0d21
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-17 18:33:33 -0300

    Input Validation and pagination

--- backend/src/app/controllers/DeliveryController.js
+++ backend/src/app/controllers/DeliveryController.js
@@ -1,17 +1,35 @@
+import * as Yup from 'yup';
 import Delivery from '../models/Delivery';
 
 class DeliveryController {
   async index(req, res) {
-    const delivery = await Delivery.findAll();
+    const { page = 1 } = req.query;
+
+    const delivery = await Delivery.findAll({
+      limit: 20,
+      offset: (page - 1) * 20,
+      attributes: ['id', 'name', 'date', 'start_point', 'end_point'],
+    });
 
     res.json(delivery);
   }
 
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      date: Yup.date().required(),
+      start_point: Yup.string().required(),
+      end_point: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { id, name, date, start_point, end_point } = req.body;
 
     const deliveryExists = await Delivery.findOne({
-      where: { name: req.body.name },
+      where: { name: req.body.name, date: req.body.date },
     });
 
     if (deliveryExists) {
