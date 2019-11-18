68bbeb16fba8c35341311bd1430e8eea89d9b6d8
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-17 23:05:12 -0300

    Pagination buttons and frotend logic

--- frontend/src/pages/List/index.js
+++ frontend/src/pages/List/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { parseISO, format } from 'date-fns';
 
@@ -6,25 +6,40 @@ import api from '../../services/api';
 
 import Header from '../../components/Header';
 
-import { Container, DeliveryTable } from './styles';
+import { Container, DeliveryTable, PageActions } from './styles';
 
 export default function List() {
   const [orders, setOrders] = useState([]);
+  const [page, setPage] = useState(1);
 
-  async function loadOrders() {
-    const response = await api.get('/deliveries');
-
+  async function loadOrders(page = 1) {
+    const response = await api.get(`/deliveries?page=${page}`);
     const data = response.data.map(order => ({
       ...order,
       dateFormatted: format(parseISO(order.date), "dd'/'M/Y"),
     }));
 
     setOrders(data);
+    setPage(page);
   }
 
   useEffect(() => {
     loadOrders();
   }, []);
+
+  const ordersSize = useMemo(() => orders.length, [orders]);
+
+  function prevPage() {
+    if (page === 1) return;
+    const pageNumber = page - 1;
+    loadOrders(pageNumber);
+  }
+
+  function nextPage() {
+    if (ordersSize < 10) return;
+    const pageNumber = page + 1;
+    loadOrders(pageNumber);
+  }
 
   return (
     <>
@@ -64,6 +79,15 @@ export default function List() {
             </tbody>
           ))}
         </DeliveryTable>
+        <PageActions>
+          <button type="button" disabled={page < 2} onClick={prevPage}>
+            Anterior
+          </button>
+          <span>Página {page}</span>
+          <button type="button" disabled={ordersSize < 10} onClick={nextPage}>
+            Próximo
+          </button>
+        </PageActions>
       </Container>
     </>
   );
