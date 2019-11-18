34b478d9bce8d9717d2da02958950333274980d1
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-18 16:01:12 -0300

    Setting the environment variables

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,12 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  port: 5435,
-  username: 'postgres',
-  password: 'docker',
-  database: 'deliveryApp',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
