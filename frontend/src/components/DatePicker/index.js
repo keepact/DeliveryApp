7e09dd376af0e92c03641de3a6692266f19f768c
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-21 22:11:56 -0300

    Moving DatePicker To Components and changing Logo

---
+++ frontend/src/components/DatePicker/index.js
@@ -0,0 +1,47 @@
+import React, { useRef, useEffect, useState } from 'react';
+import ReactDatePicker from 'react-datepicker';
+
+import { useField } from '@rocketseat/unform';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
+export default function DatePicker({ name }) {
+  const ref = useRef(null);
+  const { fieldName, registerField, defaultValue, error } = useField(name);
+  const [selected, setSelected] = useState(defaultValue);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: ref.current,
+      path: 'props.selected',
+      clearValue: pickerRef => {
+        pickerRef.clear();
+      },
+    });
+  }, [ref.current, fieldName]); // eslint-disable-line
+
+  useEffect(() => {
+    const datePickers = document.getElementsByClassName(
+      'react-datepicker__input-container'
+    );
+    Array.from(datePickers).forEach(el =>
+      el.childNodes[0].setAttribute('readOnly', true)
+    );
+  }, []);
+
+  return (
+    <>
+      <ReactDatePicker
+        name={fieldName}
+        selected={selected}
+        placeholderText="Data da entrega"
+        onChange={date => setSelected(date)}
+        ref={ref}
+      >
+        {' '}
+      </ReactDatePicker>
+      {error && <span>{error}</span>}
+    </>
+  );
+}
